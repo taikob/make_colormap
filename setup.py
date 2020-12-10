5c179fc8af249852f99c2d8ef1caa452dbbe1327
taikob <taisukekbys@gmail.com>
2020-12-10 12:08:42 +0900

    adapt to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="make_colormap",
+    version="1.0",
+    author="Taisuke Kobayashi",
+    author_email="taisukekbys@gmail.com",
+    description="make_colormap is my own python package",
+    long_description="make_colormap",
+    long_description_content_type="text/markdown",
+    url="https://github.com/taikob",
+    packages=setuptools.find_packages()
+)
